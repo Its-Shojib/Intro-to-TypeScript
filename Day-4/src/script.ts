0c5f161791283de85df9dd4f0300aba89af5c16a
Md Shojib Hossain <mdshojib922@gmail.com>
2024-07-01 12:39:36 +0600

    Person Added in Classed

--- Day-4/src/script.ts
+++ Day-4/src/script.ts
@@ -3,3 +3,19 @@
 //tsc -w
 //tsc script.ts
 //tsc
+
+// Classes
+
+class Person {
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greet(): void {
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    }
+}
