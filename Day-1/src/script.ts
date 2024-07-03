c49493ec3c8c03b721780c3a151d6f5019b6d9d3
Md Shojib Hossain <mdshojib922@gmail.com>
2024-07-04 01:57:58 +0600

    Project Setup Completed

--- Day-1/src/script.ts
+++ Day-1/src/script.ts
@@ -1,4 +1,23 @@
-console.log("Hello world!");
+    console.log("Hello world!");
 
-const country = 'Bangladesh and Myanmar';
-console.log(country);
+    const country = 'Bangladesh and Myanmar';
+    console.log(country);
+
+
+    let Player = "Mashrafi Bin Mortaza"
+
+    console.log(Player);
+
+    
+    // Player =  34; // this will produce an error message
+    
+    Player = "Tamim Iqbal Khan"
+
+    console.log(Player);
+
+    let age;
+    console.log(age); // this will log undefined
+    age = 34;
+    console.log(age); // this will log 34
+    age = "Mashrafi Bin Mortaza";
+    console.log(age); // this will log Mashrafi Bin Mortaza without any errors
